955e0d1f579348a0859d00a328dc9d263855c6d2
ardy-dylan <ntabundiardydylan@gmail.com>
2026-09-16 16:02:06 +0200

    forst commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const questions = [
+  {
+    question: "Quelle est votre niveau en javascript ?",
+    options: ["debutant", "moyen", "avance", "expert"],
+    answer:  "moyen"
+  },
+  {
+    question: "Quelle est votre niveau en html ?",
+    options: ["debutant", "moyen", "avance", "expert"],
+    answer:  "moyen"
+  },
+  {
+    question: "Quelle est votre niveau en css ?",
+    options: ["debutant", "moyen", "avance", "expert"],
+    answer:  "moyen"
+  },
+  {
+    question: "Quelle est votre niveau en Tailwind ?",
+    options: ["debutant", "moyen", "avance", "expert"],
+    answer:  "moyen"
+  },
+  {
+    question: "Quelle est votre niveau en figma ?",
+    options: ["debutant", "moyen", "avance", "expert"],
+    answer:  "moyen"
+  },
+
+
+
+
+
+]
